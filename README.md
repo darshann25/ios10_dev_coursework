c7d96ae2a744f9782a3ac6150a1f20a0661f87a0
darshan <darshanpatel25894@gmail.com>
2017-09-04 21:08:43 -0400

    Commit 1 : Initialized Repo and Started with Xcode 8 and Swift 3 Coursework : Section 2, Lecture 11

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # iOS 10 Development using Swift 3
 
-Hi guys,
-I've made this common Bitbucket Repo so that I can upload my Swift files as I go through the
-Udemy course and demonstrate how Git works. We will dive deeper into how to use it when we meet
-on Sunday / Monday.
---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Darshan Patel on 9/4/17.
+//  Copyright © 2017 dreamcatchers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional s	etup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
